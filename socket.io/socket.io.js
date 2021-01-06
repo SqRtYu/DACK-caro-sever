da9caf7bc2f115c2dc6d10fbbd21049fb73bc5a2
Tran Quoc Toan <37733986+yato511@users.noreply.github.com>
2021-01-06 16:23:10 +0700

    room flow

--- socket.io/socket.io.js
+++ socket.io/socket.io.js
@@ -3,278 +3,380 @@ const User = require("../models/user");
 let listRooms = [];
 
 module.exports = (io, socket) => {
-  socket.on("user-online", (user) => {
-    const currentUsers = Array.from(io.sockets.sockets).map(
-      ([socketId, { user }]) => ({
-        socketId,
-        ...user,
-      })
-    );
-    // if (currentUsers.filter(({ sub }) => sub && sub === user.sub).length) {
-    // 	socket.emit("force-logout", "User already logged in");
-    // } else {
-    // Send Current Online User List for the new user
-    const currentOnlineUsers = currentUsers.filter(
-      ({ sub }) => sub && sub !== user.sub
-    );
-    socket.emit("get-online", currentOnlineUsers);
-    // Send to old user that new user has just logged in
-    socket.broadcast.emit("user-online", { ...user, socketId: socket.id });
-    // Save User to Socket
-    socket.user = user;
-    // }
-  });
+	socket.on("user-online", (user) => {
+		const currentUsers = Array.from(io.sockets.sockets).map(
+			([socketId, { user }]) => ({
+				socketId,
+				...user,
+			})
+		);
+		// if (currentUsers.filter(({ sub }) => sub && sub === user.sub).length) {
+		// 	socket.emit("force-logout", "User already logged in");
+		// } else {
+		// Send Current Online User List for the new user
+		const currentOnlineUsers = currentUsers.filter(
+			({ sub }) => sub && sub !== user.sub
+		);
+		socket.emit("get-online", currentOnlineUsers);
+		socket.emit("get-rooms", listRooms);
+		// Send to old user that new user has just logged in
+		socket.broadcast.emit("user-online", { ...user, socketId: socket.id });
+		// Save User to Socket
+		socket.user = { ...user, socketId: socket.id };
+		// }
+	});
 
-  socket.on("join-room-request", (roomId, password) => {
-    // chỗ này check password các thứ
-    socket.emit("join-room-success", { roomId: room.id });
-  });
+	socket.on("join-room-request", (roomId, password) => {
+		const matched = listRooms.filter(({ id }) => id === roomId);
 
-  socket.on("create-room-request", (roomName, password, time = 30000) => {
-    console.log(
-      "create-room-request" + roomName + "password: " + password + "time: " + time
-    );
-    
-    let room = {
-      id: Date.now(),
-      roomName,
-      players: {
-        X: socket.user,
-        O: null,
-      },
-      host: socket.user.sub,
-      password,
-      time,
-      status: 1
-    };
-    listRooms.push(room);
+		if (matched.length) {
+			const room = matched[0];
 
-    socket.room = room.id;
-    socket.join(socket.room);
+			let can = false;
+			if (room.password) {
+				if (room.password === password) {
+					can = true;
+				}
+			} else can = true;
 
-    socket.emit("create-room-success", room);
+			if (can) {
+				if (room.players.O === null && room.players.X !== null)
+					room.players.O = socket.user;
+				else if (room.players.O !== null && room.players.X === null)
+					room.players.X = socket.user;
+				else return;
 
-    io.emit("has-new-room", room);
+				socket.room = room.id;
+				socket.roomInfo = room;
+				socket.join(socket.room);
+				socket.emit("join-room-success", room);
+				io.to(room.host.socketId).emit("room-detail-update", room);
+				//
+				io.emit("room-list-update-room", room);
+			}
+		}
+	});
 
-    console.log("Room [" + socket.room + "] created");
-  });
+	socket.on("create-room-request", (roomName, password, time = 30000) => {
+		console.log(
+			"create-room-request" +
+				roomName +
+				"password: " +
+				password +
+				"time: " +
+				time
+		);
 
-  socket.on("out-game", () => {
-    console.log("out");
+		let room = {
+			id: Date.now(),
+			roomName,
+			players: {
+				X: socket.user,
+				O: null,
+			},
+			host: socket.user,
+			password,
+			time,
+			status: 1,
+		};
+		listRooms.push(room);
 
-    socket.leave(socket.room);
+		socket.room = room.id;
+		socket.roomInfo = room;
+		socket.join(socket.room);
 
-    for (let i = 0; i < listRooms.length; i++) {
-      if (listRooms[i].id == socket.room) {
-        if (listRooms[i].playerO == null) {
-          listRooms.splice(i, 1);
-          console.log("Room [" + socket.room + "] destroyed");
-        } else {
-          console.log(socket.data.name);
-          if (listRooms[i].playerO === socket.data.name) {
-            listRooms[i].playerO = "DISCONNECTED";
-          }
-          if (listRooms[i].playerX === socket.data.name) {
-            listRooms[i].playerX = "DISCONNECTED";
-          }
+		socket.emit("create-room-success", room);
 
-          if (
-            listRooms[i].playerO === "DISCONNECTED" &&
-            listRooms[i].playerX === "DISCONNECTED"
-          ) {
-            listRooms.splice(i, 1);
-            console.log("Room [" + socket.room + "] destroyed");
-          } else {
-            io.to(listRooms[i].id).emit("disconnectRoom", listRooms[i]);
-            console.log(
-              "Player [" +
-                socket.data.name +
-                "] leave room [" +
-                socket.room +
-                "]"
-            );
-          }
-        }
+		io.emit("has-new-room", room);
 
-        break;
-      }
-    }
-  });
+		console.log("Room [" + socket.room + "] created");
+	});
 
-  socket.on("disconnect", () => {
-    console.log("disconnect");
+	socket.on("invite-room", (socketId) => {
+		io.to(socketId).emit("receive-invite-request", socket.roomInfo);
+	});
 
-    socket.broadcast.emit("user-offline", socket.id);
-    // socket.removeAllListeners();
+	socket.on("reply-invite-request", ({ isAccept, roomInfo: room }) => {
+		if (isAccept) {
+			if (room.players.O === null && room.players.X !== null)
+				room.players.O = socket.user;
+			else if (room.players.O !== null && room.players.X === null)
+				room.players.X = socket.user;
+			else return;
 
-    socket.leave(socket.room);
+			socket.room = room.id;
+			socket.roomInfo = room;
+			socket.join(socket.room);
+			socket.emit("join-room-success", room);
+			io.emit("room-list-update-room", room);
+			io.to(room.host.socketId).emit("room-detail-update", room);
+			io.to(room.host.socketId).emit("accept-invite-request", socket.user);
+		} else {
+			io.to(room.host.socketId).emit("decline-invite-request", socket.user);
+		}
+	});
 
-    for (let i = 0; i < listRooms.length; i++) {
-      if (listRooms[i].id == socket.room) {
-        if (listRooms[i].playerO == null) {
-          listRooms.splice(i, 1);
-          console.log("Room [" + socket.room + "] destroyed");
-        } else {
-          console.log(socket.data.name);
-          if (listRooms[i].playerO === socket.data.name) {
-            listRooms[i].playerO = "DISCONNECTED";
-          }
-          if (listRooms[i].playerX === socket.data.name) {
-            listRooms[i].playerX = "DISCONNECTED";
-          }
+	socket.on("leave-room", () => {
+		const isPlayerX =
+			socket.roomInfo.players.X &&
+			socket.user.sub === socket.roomInfo.players.X.sub;
 
-          if (
-            listRooms[i].playerO === "DISCONNECTED" &&
-            listRooms[i].playerX === "DISCONNECTED"
-          ) {
-            listRooms.splice(i, 1);
-            console.log("Room [" + socket.room + "] destroyed");
-          } else {
-            io.to(listRooms[i].id).emit("disconnectRoom", listRooms[i]);
-            console.log(
-              "Player [" +
-                socket.data.name +
-                "] leave room [" +
-                socket.room +
-                "]"
-            );
-          }
-        }
+		const isHost =
+			socket.roomInfo.host && socket.roomInfo.host.sub === socket.user.sub;
 
-        break;
-      }
-    }
-  });
+		// neu no la host
+		if (isHost) {
+			// neu no la X va la host
+			if (isPlayerX) {
+				socket.roomInfo.host = socket.roomInfo.players.O;
+				socket.roomInfo.players.X = null;
+			}
+			// neu no la host va la O
+			else {
+				socket.roomInfo.host = socket.roomInfo.players.X;
+				socket.roomInfo.players.O = null;
+			}
+		} else {
+			if (isPlayerX) socket.roomInfo.players.X = null;
+			else socket.roomInfo.players.O = null;
+		}
+		if (
+			socket.roomInfo.players.X === null &&
+			socket.roomInfo.players.O === null
+		) {
+			console.log("Huy room ", socket.room);
+			// xoa ra khoi list
+			listRooms = listRooms.filter((room) => room.id !== socket.room);
+			// xoa room
+			io.emit("room-list-delete-room", socket.roomInfo.id);
+		} else {
+			// thong bao cho room
+			io.to(socket.room).emit("room-detail-update", socket.roomInfo);
+			// thong bao cho moi nguoi
+			io.emit("room-list-update-room", socket.roomInfo);
+		}
 
-  socket.on("join-room-quick", (data) => {
-    console.log("join-room-quick");
-    socket.data = data;
+		socket.leave(socket.room);
 
-    for (let i = 0; i < listRooms.length; i++) {
-      if (listRooms[i].playerO == null) {
-        listRooms[i].playerO = data.name;
-        listRooms[i].pictureO = data.picture;
-        socket.room = listRooms[i].id;
-        socket.join(socket.room);
+		delete socket.room;
+		delete socket.roomInfo;
+	});
 
-        io.in(listRooms[i].id).emit("join-room-quick-success", listRooms[i]);
+	socket.on("start-game-request", () => {
+		io.to(socket.room).emit("start-game");
+	});
 
-        console.log("Room [" + socket.room + "] played");
-        return;
-      }
-    }
+	socket.on("out-game", () => {
+		console.log("out");
 
-    let room = {
-      id: Date.now(),
-      playerX: data.name,
-      pictureX: data.picture,
-      playerO: null,
-      pictureO: null,
-    };
-    listRooms.push(room);
+		socket.leave(socket.room);
 
-    socket.room = room.id;
-    socket.join(socket.room);
+		let room = listRooms.find((room) => room.id === socket.room);
+		if (room.length) {
+			room = room[0];
+			// neu no la X va thang O ko con
+			if (room.players.O == null) {
+				listRooms = listRooms.filter((room) => room.id !== socket.room);
+				console.log("Room [" + socket.room + "] destroyed");
+			} else {
+				// Neu no la X va thang O con
+				// Neu no la O
+				if (room.players.O.sub === socket.user.sub) {
+					room.players.O = null;
+				}
+				if (room.players.X.sub === socket.user.sub) {
+					room.players.X = null;
+				}
 
-    console.log("Room [" + socket.room + "] created");
-  });
+				if (room.players.O === null && room.players.X === null) {
+					listRooms = listRooms.filter((room) => room.id !== socket.room);
+					console.log("Room [" + socket.room + "] destroyed");
+				} else {
+					io.to(room.id).emit("disconnectRoom", room);
+					console.log(
+						"Player [" +
+							socket.user.nickname +
+							"] leave room [" +
+							socket.room +
+							"]"
+					);
+				}
+			}
+		}
+	});
 
-  socket.on("move", (data) => {
-    console.log("move");
-    console.log(socket.id);
-    socket.to(socket.room).emit("move", data);
+	socket.on("disconnect", () => {
+		console.log("disconnect");
 
-    for (let i = 0; i < listRooms.length; i++) {
-      if (listRooms[i].id == socket.room) {
-        listRooms[i].lastMove = data;
-      }
-    }
-  });
+		socket.broadcast.emit("user-offline", socket.id);
+		// socket.removeAllListeners();
 
-  socket.on("chat", (data) => {
-    console.log("chat" + data);
-    socket.emit("chat", {
-      sender: socket.data.name,
-      message: data,
-    });
-    socket.to(socket.room).emit("chat", {
-      sender: socket.data.name,
-      message: data,
-    });
-  });
+		socket.leave(socket.room);
 
-  //refresh-game
-  socket.on("refresh-game-request", (data) => {
-    socket.to(socket.room).emit("refresh-game-request", data);
-  });
+		let room = listRooms.find((room) => room.id === socket.room);
+		console.log(room);
+		if (room && room.length) {
+			room = room[0];
+			// neu no la X va thang O ko con
+			if (room.players.O == null) {
+				listRooms = listRooms.filter((room) => room.id !== socket.room);
+				console.log("Room [" + socket.room + "] destroyed");
+			} else {
+				// Neu no la X va thang O con
+				// Neu no la O
+				if (room.players.O.sub === socket.user.sub) {
+					room.players.O = null;
+				}
+				if (room.players.X.sub === socket.user.sub) {
+					room.players.X = null;
+				}
 
-  socket.on("refresh-game-result", (data) => {
-    socket.to(socket.room).emit("refresh-game-result", data);
-  });
+				if (room.players.O === null && room.players.X === null) {
+					listRooms = listRooms.filter((room) => room.id !== socket.room);
+					console.log("Room [" + socket.room + "] destroyed");
+				} else {
+					io.to(room.id).emit("disconnectRoom", room);
+					console.log(
+						"Player [" +
+							socket.user.nickname +
+							"] leave room [" +
+							socket.room +
+							"]"
+					);
+				}
+			}
+		}
+	});
 
-  // surrender
-  socket.on("surrender-request", () => {
-    socket.to(socket.room).emit("surrender-request");
-  });
+	socket.on("join-room-quick", (data) => {
+		console.log("join-room-quick");
+		socket.data = data;
 
-  socket.on("surrender-result", () => {
-    socket.to(socket.room).emit("surrender-result");
-  });
+		for (let i = 0; i < listRooms.length; i++) {
+			if (listRooms[i].playerO == null) {
+				listRooms[i].playerO = data.name;
+				listRooms[i].pictureO = data.picture;
+				socket.room = listRooms[i].id;
+				socket.join(socket.room);
 
-  // draw
-  socket.on("draw-request", () => {
-    console.log("draw-request");
-    socket.to(socket.room).emit("draw-request");
-  });
+				io.in(listRooms[i].id).emit("join-room-quick-success", listRooms[i]);
 
-  socket.on("draw-result", (data) => {
-    console.log("draw-result");
-    socket.to(socket.room).emit("draw-result", data);
-  });
+				console.log("Room [" + socket.room + "] played");
+				return;
+			}
+		}
 
-  // socket.on("on-reconnect", function (data) {
-  // 	if (data.roomInfo) {
-  // 		socket.data = data.user;
+		let room = {
+			id: Date.now(),
+			playerX: data.name,
+			pictureX: data.picture,
+			playerO: null,
+			pictureO: null,
+		};
+		listRooms.push(room);
 
-  // 		for (var i = 0; i < listRooms.length; i++) {
-  // 			if (listRooms[i].id === data.roomInfo.id) {
-  // 				if (listRooms[i].playerO === "DISCONNECTED") {
-  // 					listRooms[i].playerO = data.user.name;
-  // 					listRooms[i].pictureO = data.user.picture;
-  // 				}
-  // 				if (listRooms[i].playerX === "DISCONNECTED") {
-  // 					listRooms[i].playerX = data.user.name;
-  // 					listRooms[i].pictureX = data.user.picture;
-  // 				}
+		socket.room = room.id;
+		socket.join(socket.room);
 
-  // 				socket.room = listRooms[i].id;
-  // 				socket.join(socket.room);
+		console.log("Room [" + socket.room + "] created");
+	});
 
-  // 				socket.to(socket.room).emit("on-reconnect", listRooms[i]);
-  // 				console.log(
-  // 					"Player [" +
-  // 						data.user.name +
-  // 						"] reconnected in room [" +
-  // 						socket.room +
-  // 						"]"
-  // 				);
+	socket.on("move", (data) => {
+		console.log("move");
+		console.log(socket.id);
+		socket.to(socket.room).emit("move", data);
 
-  // 				if (listRooms[i].lastMove) {
-  // 					socket.emit("move", listRooms[i].lastMove);
-  // 				}
+		for (let i = 0; i < listRooms.length; i++) {
+			if (listRooms[i].id == socket.room) {
+				listRooms[i].lastMove = data;
+			}
+		}
+	});
 
-  // 				return;
-  // 			}
-  // 		}
+	socket.on("chat", (data) => {
+		console.log("chat" + data);
+		socket.emit("chat", {
+			sender: socket.data.name,
+			message: data,
+		});
+		socket.to(socket.room).emit("chat", {
+			sender: socket.data.name,
+			message: data,
+		});
+	});
 
-  // 		socket.emit("on-reconnect", null);
-  // 		console.log(
-  // 			"Player [" +
-  // 				data.user.name +
-  // 				"] find room [" +
-  // 				data.roomInfo.id +
-  // 				"] but not exists"
-  // 		);
-  // 	}
-  // });
+	//refresh-game
+	socket.on("refresh-game-request", (data) => {
+		socket.to(socket.room).emit("refresh-game-request", data);
+	});
+
+	socket.on("refresh-game-result", (data) => {
+		socket.to(socket.room).emit("refresh-game-result", data);
+	});
+
+	// surrender
+	socket.on("surrender-request", () => {
+		socket.to(socket.room).emit("surrender-request");
+	});
+
+	socket.on("surrender-result", () => {
+		socket.to(socket.room).emit("surrender-result");
+	});
+
+	// draw
+	socket.on("draw-request", () => {
+		console.log("draw-request");
+		socket.to(socket.room).emit("draw-request");
+	});
+
+	socket.on("draw-result", (data) => {
+		console.log("draw-result");
+		socket.to(socket.room).emit("draw-result", data);
+	});
+
+	// socket.on("on-reconnect", function (data) {
+	// 	if (data.roomInfo) {
+	// 		socket.data = data.user;
+
+	// 		for (var i = 0; i < listRooms.length; i++) {
+	// 			if (listRooms[i].id === data.roomInfo.id) {
+	// 				if (listRooms[i].playerO === "DISCONNECTED") {
+	// 					listRooms[i].playerO = data.user.name;
+	// 					listRooms[i].pictureO = data.user.picture;
+	// 				}
+	// 				if (listRooms[i].playerX === "DISCONNECTED") {
+	// 					listRooms[i].playerX = data.user.name;
+	// 					listRooms[i].pictureX = data.user.picture;
+	// 				}
+
+	// 				socket.room = listRooms[i].id;
+	// 				socket.join(socket.room);
+
+	// 				socket.to(socket.room).emit("on-reconnect", listRooms[i]);
+	// 				console.log(
+	// 					"Player [" +
+	// 						data.user.name +
+	// 						"] reconnected in room [" +
+	// 						socket.room +
+	// 						"]"
+	// 				);
+
+	// 				if (listRooms[i].lastMove) {
+	// 					socket.emit("move", listRooms[i].lastMove);
+	// 				}
+
+	// 				return;
+	// 			}
+	// 		}
+
+	// 		socket.emit("on-reconnect", null);
+	// 		console.log(
+	// 			"Player [" +
+	// 				data.user.name +
+	// 				"] find room [" +
+	// 				data.roomInfo.id +
+	// 				"] but not exists"
+	// 		);
+	// 	}
+	// });
 };
